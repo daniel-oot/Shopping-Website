139aff1596ed441726b39b413f8b8915d6738dc5
Daniel-oot <omoijiadedaniel0@gmail.com>
2024-09-07 16:11:05 -0500

    New Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { CartComponent } from './cart/cart.component';
+import { WomenComponent } from './shop/women/women.component';
+import { KidsComponent } from './shop/kids/kids.component';
+import { MenComponent } from './shop/men/men.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'shop',component:ShopComponent,
+  children: [
+    { path: 'men', component: MenComponent },
+    { path: 'women', component: WomenComponent },
+    { path: 'kids', component: KidsComponent }
+  ]},
+  {path:'favorite',component:FavoriteComponent},
+  {path:'cart',component:CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { CartComponent } from './cart/cart.component';
+import { MenComponent } from './shop/men/men.component';
+import { WomenComponent } from './shop/women/women.component';
+import { KidsComponent } from './shop/kids/kids.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    FavoriteComponent,
+    CartComponent,
+    MenComponent,
+    WomenComponent,
+    KidsComponent
   ],
   imports: [
     BrowserModule,
